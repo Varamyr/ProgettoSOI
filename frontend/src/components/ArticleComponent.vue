362bb340698794c3c34917e246157ebb08261125
Varamyr <xxvitto94xx@hotmail.it>
2020-12-19 19:11:03 +0100

    Card group aggiustato

--- frontend/src/components/ArticleComponent.vue
+++ frontend/src/components/ArticleComponent.vue
@@ -5,7 +5,7 @@
     <hr>
     <p class="error" v-if="error">{{error}}</p>
     <div class="articles-container">
-        <b-card-group deck style="justify-content: center;">
+        <div class="card-group" style="justify-content:center;">
           <b-card
             no-body
             class="m-2 "
@@ -16,7 +16,7 @@
             v-bind:key="article._id">
             <div class="row h-100">
               <b-col sm="6">
-                <b-img class="h-100 embed-responsive" style="object-fit: cover;" fluid src="https://picsum.photos/1024/400/?image=41" alt="Image 1"></b-img>
+                <b-img class="h-100 embed-responsive" style="object-fit: cover;" fluid src="https://images.everyeye.it/img-notizie/nvidia-geforce-rtx-3090-3080-3070-trapelano-specifiche-prezzi-v3-465276.jpg" alt="Image 1"></b-img>
               </b-col>
               <b-col sm="6" class="py-3">
                 <h4>{{article.nomeArticolo}}</h4>
@@ -32,7 +32,7 @@
               </b-col>
             </div>
           </b-card>
-        </b-card-group>
+        </div>
     </div>
   </div>
 </template>
