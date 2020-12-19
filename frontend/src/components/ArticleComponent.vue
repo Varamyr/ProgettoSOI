3a3d9eaae28b3edc05e11ffcbf7b9604b3d632b1
Varamyr <xxvitto94xx@hotmail.it>
2020-12-19 16:28:48 +0100

    Aggiunto frontend ai file di git

---
+++ frontend/src/components/ArticleComponent.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="container">
+    <h1>Articoli in vendita</h1>
+    <!-- Creare gli articoli qui-->
+    <hr>
+    <p class="error" v-if="error">{{error}}</p>
+    <div class="articles-container">
+        <b-card-group deck style="justify-content: center;">
+          <b-card
+            no-body
+            class="m-2 "
+            style="max-width: 450px; min-width:450px"
+            v-for="(article, index) in articles"
+            v-bind:item="article"
+            v-bind:index="index"
+            v-bind:key="article._id">
+            <div class="row h-100">
+              <b-col sm="6">
+                <b-img class="h-100 embed-responsive" style="object-fit: cover;" fluid src="https://picsum.photos/1024/400/?image=41" alt="Image 1"></b-img>
+              </b-col>
+              <b-col sm="6" class="py-3">
+                <h4>{{article.nomeArticolo}}</h4>
+                <b-card-text>
+                  {{article.descrizione}}
+                </b-card-text>
+                <b-card-text>
+                  Prezzo: <h4>{{article.prezzo}} $</h4>
+                </b-card-text>
+                <b-card-text>
+                  Venditore: {{article.venditore}}
+                </b-card-text>
+              </b-col>
+            </div>
+          </b-card>
+        </b-card-group>
+    </div>
+  </div>
+</template>
+
+<script>
+import ArticleService from '../ArticleService'
+
+export default {
+  name: 'ArticleComponent',
+  data() {
+    return {
+      articles: [],
+      error : ''
+    }
+  },
+  async created() {
+    try{
+      this.articles = await ArticleService.getArticles();
+    }catch(err){
+      this.error = err.message;
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+div.container {
+  max-width: 1000px;
+  min-width: 400px;
+  margin: 0 auto;
+}
+
+p.error{
+  border: 1px solid #ff5b5f;
+  background-color: #ff5bc1;
+  padding: 10px;
+  margin-bottom: 15px;
+}
+</style>
